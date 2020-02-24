96f5a3ef863ed325d5c66b4a1016ce9af7b68254
kschmall <schmallk@moravian.edu>
2020-02-24 16:49:34 -0500

    Fixed .py files for pylint

--- src/c20_server/numbers.py
+++ src/c20_server/numbers.py
@@ -1,3 +1,4 @@
+
 class Numbers:
 
 	def get_value(self):
--- tests/test_numbers.py
+++ tests/test_numbers.py
@@ -1,5 +1,5 @@
 from c20_server.numbers import Numbers
 
-    def test_get_value_return_():
-        numbers = Numbers()
-        assert.numbers.get_value() == 100
+def test_get_value_return_():
+    numbers = Numbers()
+    assert numbers.get_value() == 100
