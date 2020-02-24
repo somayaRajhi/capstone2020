64d4d3dfb8b3ca7e65018419019636006e34bea8
kschmall <schmallk@moravian.edu>
2020-02-24 17:34:32 -0500

    Fix py files to pass pylint

--- src/c20_server/numbers.py
+++ src/c20_server/numbers.py
@@ -1,5 +1,21 @@
+"""
+Module
+"""
 
 class Numbers:
+    """
+    There.
+    """
+    value = 100
 
-	def get_value(self):
-		return 100
+    def get_value(self):
+        """
+        Hi.
+        """
+        return self.value
+
+    def set_value(self, value):
+        """
+        Hi.
+        """
+        self.value = value
--- tests/test_numbers.py
+++ tests/test_numbers.py
@@ -1,5 +1,11 @@
+'''
+Module.
+'''
 from c20_server.numbers import Numbers
 
 def test_get_value_return_():
+    '''
+    Test.
+    '''
     numbers = Numbers()
     assert numbers.get_value() == 100
