1b0c5e5009e6aa55ba23321d568c7fb82eef7ac3
kschmall <schmallk@moravian.edu>
2020-02-24 17:38:14 -0500

    Fix test_numbers

--- tests/test_numbers.py
+++ tests/test_numbers.py
@@ -9,3 +9,11 @@ def test_get_value_return_():
     '''
     numbers = Numbers()
     assert numbers.get_value() == 100
+
+def test_set_value_():
+    '''
+    Test
+    '''
+    numbers = Numbers()
+    numbers.set_value(10)
+    assert numbers.get_value() == 10
