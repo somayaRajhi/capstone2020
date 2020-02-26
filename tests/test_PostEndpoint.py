f21f812a00da0b5fa68d93bd160c5c695e07904a
Somaya Rajhi <stsar04@moravian.edu>
2020-02-26 11:13:35 -0500

    add two test when the job success and unsuccess

--- tests/test_PostEndpoint.py
+++ tests/test_PostEndpoint.py
@@ -10,3 +10,17 @@ def empty_json():
 
     result = client.get('/empty_json')
     assert result.status_code == 200
+
+def get_job_success():
+    app.config['TESTING'] = True
+    client = app.test_client()
+
+    result = client.get('/get_job')
+    assert result.status_code == 200
+
+def get_job_unsuccess():
+        app.config['TESTING'] = True
+        client = app.test_client()
+
+        result = client.get('/get_job')
+        assert result.status_code == 404
