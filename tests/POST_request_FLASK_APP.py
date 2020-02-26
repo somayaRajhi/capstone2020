e9e74a078a5198f1c19e987bfc4bb754d4bbcbad
Yuwen Wang <60441151+ywwgu@users.noreply.github.com>
2020-02-26 12:45:12 -0500

    remove unnecessary code and add test_json method
    will need json method in /src

--- tests/POST_request_FLASK_APP.py
+++ tests/POST_request_FLASK_APP.py
@@ -4,24 +4,12 @@ app = Flask(__name__)
 
 
 @app.route('/json', methods=['POST'])
-def json():
-    req_data = request.get_json()
+def test_json():
+	
+    app.config['TESTING'] = True
+    client = app.test_client()
 
-    language = req_data['language']
-    framework = req_data['framework']
-    python_version = req_data['version_info']['python']
-    example = req_data['examples'][0]
-    boolean_test = req_data['boolean_test']
-
-    return {}, 200
+    result = client.get('/json')
+    assert '200' in result.data
 
 
-''' json job ID
-	error or pass code
-	test1 correct data
-	test2 error data
-	status code'''
-
-
-if __name__ == '__main__':
-     app.run(debug=True, port=5000)
