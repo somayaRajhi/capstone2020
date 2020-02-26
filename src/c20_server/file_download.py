ebca6ab5cc9c6cd5c8f511e61b3c4e0deebd1779
elijahives <ivese@moravian.edu>
2020-02-26 14:17:26 -0500

    fix syntax

--- src/c20_server/file_download.py
+++ src/c20_server/file_download.py
@@ -4,11 +4,14 @@ import os
 import requests
 
 
-def download_file(URL, api_key, documentId = ""):
-    if(documentId == ""):
-        data = requests.get(url=URL + api_key + '&rpp=1')
+def download_file(url, api_key, document_id=""):
+    """
+    downloads a file based on a url, api key and document_id (if given)
+    """
+    if document_id == "":
+        data = requests.get(url + api_key + '&rpp=1')
     else:
-        data = requests.get(url=URL + api_key + "documentId=" + documentId)
+        data = requests.get(url + api_key + "documentId=" + document_id)
     if data.status_code == 403:
         raise customError.IncorrectApiKey
     if data.status_code == 429:
@@ -24,6 +27,7 @@ def main():
     api_key = os.getenv("API_KEY")
     result = download_file('https://api.data.gov:443/regulations/v3/documents.json?', 'api_key=' + api_key)
     print(result)
+
 
 if __name__ == "__main__":
     main()
