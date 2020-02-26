3fbefc94ead3cf0a548a4c6dec0b01665b9aeeac
Somaya Rajhi <stsar04@moravian.edu>
2020-02-26 15:04:33 -0500

    new change in get_job functions

--- src/c20_server/PostEndpoint.py
+++ src/c20_server/PostEndpoint.py
@@ -11,11 +11,10 @@ def empty_json():
             abort(400)
     return json.dumps({}),200
 
-
-
 @app.route('/get_job', methods=['POST'])
 def get_work():
     json_data=rerequest.form['json']
+    ##json_data=request.args.get['json_data']////
     if json_data is None:
         return abort(400)
 
