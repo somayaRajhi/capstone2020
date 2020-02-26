cd2f205e3b6eab10bc5f745105f73b6d33d4f839
Somaya Rajhi <stsar04@moravian.edu>
2020-02-26 11:10:12 -0500

    new changes in both functions

--- src/c20_server/PostEndpoint.py
+++ src/c20_server/PostEndpoint.py
@@ -1,17 +1,22 @@
-from flask import Flask, jsonify, request #import objects from the Flask model
-app = Flask(__name__) #define app using Flask
+from flask import Flask, abort, request #import objects from the Flask model
+import json
 
+
+app = Flask(__name__) #define app using Flask
 
 
 @app.route('/empty_json')
 def empty_json():
-    return jsonify ({})
+        if not request.json:
+            abort(400)
+    return json.dumps({}),200
 
 
 
-@app.route('/get_work', methods=['POST'])
-def get_work();
- if not request.json;
-        abort(400)
+@app.route('/get_job', methods=['POST'])
+def get_work():
+    json_data=rerequest.form['json']
+    if json_data is None:
+        return abort(400)
 
-return jsonify({}), 200
+    return json.dumps({json_data}), 200
