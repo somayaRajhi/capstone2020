24fff7c8c34ca2454a81c6d8081e3c0864aa8e5c
Somaya Rajhi <stsar04@moravian.edu>
2020-02-28 08:41:00 -0500

    just factoring code

--- tests/test_PostEndpoint.py
+++ tests/test_PostEndpoint.py
@@ -1,26 +1,26 @@
-
-
-
 from c20_server.PostEndpoint import app
+import pytest
 
 
-def empty_json():
+
+@pytest.fixture
+def client():
     app.config['TESTING'] = True
     client = app.test_client()
+    yield client
 
+
+
+
+
+def test_empty_json(client):
     result = client.get('/empty_json')
     assert result.status_code == 200
 
-def get_job_success():
-    app.config['TESTING'] = True
-    client = app.test_client()
-
+def test_get_job_success(client):
     result = client.get('/get_job')
     assert result.status_code == 200
 
-def get_job_unsuccess():
-        app.config['TESTING'] = True
-        client = app.test_client()
-
+def test_get_job_unsuccess(client):
         result = client.get('/get_job')
         assert result.status_code == 404
