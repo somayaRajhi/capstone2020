a9c21ddba3b2a0509972d4880fdc2a355d7bb17a
Caelin Finn McCool <mccoolc@moravian.edu>
2020-02-28 15:33:32 -0500

    Refactor tests to use fixture

--- tests/get_job_test.py
+++ tests/get_job_test.py
@@ -1,23 +1,30 @@
 from c20_server.get_job import app
+import pytest
 
 
-def test_connection():
-    response = app.test_client().get('/')
+@pytest.fixture()
+def client():
+    app.config['TESTING'] = True
+    return app.test_client()
+
+
+def test_connection(client):
+    response = client.get('/')
     assert response.status_code == 200
 
 
-def test_getJob():
-    response = app.test_client().get('/')
+def test_get_job(client):
+    response = client.get('/')
     assert response.data == b'{"docketId":"CMS-2014-0115"}\n'
 
 
-def test_url():
-    response2 = app.test_client().get('/job/')
-    assert response2.status_code == 404
+def test_url(client):
+    response = client.get('/job/')
+    assert response.status_code == 404
 
 
-def test_user():
-    response3 = app.test_client().get('/request/CMS')
-    assert response3.data == b'{"Success":"Access Granted"}\n'
+def test_user(client):
+    response = client.get('/request/CMS')
+    assert response.data == b'{"Success":"Access Granted"}\n'
 
 
