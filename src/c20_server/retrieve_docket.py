62f6e0574c1eb7a102932e37fb3c3144fa82f1a8
anthonyestephan <estephana02@moravian.edu>
2020-02-28 15:43:10 -0500

    test with mock

--- src/c20_server/retrieve_docket.py
+++ src/c20_server/retrieve_docket.py
@@ -1,30 +1,24 @@
 """
 Contains class used to retreive dockets from regulations.gov
 """
-import os
 import json
 import requests
 
-
-class RetrieveDocket:
+def jformat(obj):
     """
-    Retrieve the docket in a neat JSON format
+    Create formatted string of JSON object
     """
-
-    def __jformat(self, obj):
-        """
-        Create formatted string of JSON object
-        """
-        formatted = json.dumps(obj, sort_keys=True, indent=4)
-        return formatted
+    formatted = json.dumps(obj, sort_keys=True, indent=4)
+    return formatted
 
 
-    def get_docket(self, docket_id):
-        """
-        Makes call to regulations.gov and retrieves the docket
-        """
-        api_key = os.getenv('API_Key')
-        response = requests.get("https://api.data.gov:443/regulations/v3/docket" +
-                                ".json?api_key=" + api_key + "&docketId=" + docket_id)
+def get_docket(api_key, docket_id):
+    """
+    Makes call to regulations.gov and retrieves the docket
+    """
+    response = requests.get("https://api.data.gov:443/regulations/v3/docket.json?api_key=" +
+                            api_key +
+                            "&docketId=" +
+                            docket_id)
 
-        return self.__jformat(response.json())
+    return jformat(response.json())
--- tests/test_retrieve_docket.py
+++ tests/test_retrieve_docket.py
@@ -2,21 +2,32 @@
 Test the retrieve_docket.py file
 """
 import os
+from dotenv import load_dotenv, find_dotenv
 import json
 import requests
+import requests_mock
+from c20_server.retrieve_docket import get_docket
 
-from c20_server.retrieve_docket import RetrieveDocket
+URL = "https://api.data.gov:443/regulations/v3/docket.json?api_key="
+load_dotenv(find_dotenv())
+API_KEY = os.getenv("API_KEY")
+DOCKET_ID = "EPA-HQ-OAR-2011-0028"
 
-def create_an_instance_of_object():
-    a_test = RetrieveDocket()
-    assert a_test is not None
+def test_mock_server():
+    with requests_mock.Mocker() as mock:
+        mock.get(URL + API_KEY + "&docketID=" + DOCKET_ID, json='resp')
+        response = get_docket(API_KEY, DOCKET_ID)
 
-def test_good_docket():
-    a_test = RetrieveDocket()
+        assert response == '"resp"'
 
-    api_key = os.getenv('API_Key')
+
+def test_good_docket_id():
+
     response = requests.get("https://api.data.gov:443/regulations/v3/docket" +
-                            ".json?api_key=" + api_key + "&docketId=EPA-HQ-OAR-2011-0028")
+                            ".json?api_key=" + API_KEY + "&docketId=EPA-HQ-OAR-2011-0028")
 
     formatted = json.dumps(response.json(), sort_keys=True, indent=4)
-    assert a_test.get_docket('EPA-HQ-OAR-2011-0028') == formatted
+    assert get_docket('EPA-HQ-OAR-2011-0028') == formatted
+
+def test_bad_docket_id():
+    pass
