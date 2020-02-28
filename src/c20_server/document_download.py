69a99716f6242ad0d89cdb8b8386f690fe00b12c
student <student@ml-studentloaner5.students.moravian.edu>
2020-02-28 14:23:36 -0500

    refactor the file name and function arguments

--- src/c20_server/document_download.py
+++ src/c20_server/document_download.py
@@ -1,23 +1,26 @@
-from c20_server import customError
+from c20_server import regulations_api_errors
 import requests
 
 
-def download_file(url, api_key, document_id=""):
+def download_document(api_key, document_id=""):
     """
     downloads a file based on a url, api key and document_id (if given)
     """
+    url = "https://api.data.gov:443/regulations/v3/documents.json?"
     if document_id == "":
         data = requests.get(url + api_key + '&rpp=1')
     else:
         data = requests.get(url + api_key + "documentId=" + document_id)
     if data.status_code == 403:
-        raise customError.IncorrectApiKey
+        raise regulations_api_errors.IncorrectApiKey
     if data.status_code == 429:
-        raise customError.ThousandCalls
+        raise regulations_api_errors.ThousandCalls
     if data.status_code == 404:
-        raise customError.BadID
+        raise regulations_api_errors.BadID
     document = data.json()
     return document
+
+
 
 
 
