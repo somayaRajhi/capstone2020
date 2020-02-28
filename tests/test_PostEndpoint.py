8597aacb4bf77c39111bdf874bf1e53f11577bce
Somaya Rajhi <stsar04@moravian.edu>
2020-02-28 13:15:27 -0500

    change in client get syntax

--- tests/test_PostEndpoint.py
+++ tests/test_PostEndpoint.py
@@ -1,6 +1,6 @@
 from c20_server.PostEndpoint import app
 import pytest
-
+import json
 
 
 @pytest.fixture
@@ -9,23 +9,16 @@ def client():
     client = app.test_client()
     yield client
 
+
+
 def test_empty_json(client):
     result = client.get('/empty_json')
     assert result.status_code == 200
 
 def test_get_job_success(client):
-        result = client.get('/get_job')
+        result = client.get('get_job?clinet_id=soma')
         assert result.status_code == 200
 
 def test_get_job_unsuccess(client):
-        result = client.get('/get_job')
-        assert result.status_code == 404
-
-def test_get_post_success(client):
-        result = client.post('/post')
-        assert result.status_code == 200
-
-
-def test_get_post_unsuccess(client):
-        result = client.post('/post')
-        assert result.status_code == 405
+        result = client.get('/get_job?clinet_id=bas_id')
+        assert result.status_code == 403
