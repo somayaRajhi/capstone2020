2d10bc64a42e39b3669a6b0b09d1713d852ed241
anthonyestephan <estephana02@moravian.edu>
2020-03-04 19:03:33 -0500

    add mock tests

--- tests/test_retrieve_docket.py
+++ tests/test_retrieve_docket.py
@@ -1,33 +1,76 @@
 """
 Test the retrieve_docket.py file
 """
-import os
-from dotenv import load_dotenv, find_dotenv
 import json
 import requests
 import requests_mock
+import pytest
 from c20_server.retrieve_docket import get_docket
+from c20_server import reggov_api_docket_error
 
 URL = "https://api.data.gov:443/regulations/v3/docket.json?api_key="
-load_dotenv(find_dotenv())
-API_KEY = os.getenv("API_KEY")
+API_KEY = "VALID KEY"
 DOCKET_ID = "EPA-HQ-OAR-2011-0028"
 
-def test_mock_server():
+
+def test_good_response():
     with requests_mock.Mocker() as mock:
-        mock.get(URL + API_KEY + "&docketID=" + DOCKET_ID, json='resp')
+        mock.get(URL + API_KEY + "&docketID=" + DOCKET_ID, json='The test is successful')
         response = get_docket(API_KEY, DOCKET_ID)
 
-        assert response == '"resp"'
+        assert response == '"The test is successful"'
 
-
-def test_good_docket_id():
-
-    response = requests.get("https://api.data.gov:443/regulations/v3/docket" +
-                            ".json?api_key=" + API_KEY + "&docketId=EPA-HQ-OAR-2011-0028")
-
-    formatted = json.dumps(response.json(), sort_keys=True, indent=4)
-    assert get_docket('EPA-HQ-OAR-2011-0028') == formatted
 
 def test_bad_docket_id():
-    pass
+    with requests_mock.Mocker() as mock:
+        bad_docket = DOCKET_ID + '-0101'
+        mock.get(URL + API_KEY + "&docketID=" + bad_docket,
+                 json='The test should yield a bad id', status_code=404)
+
+        with pytest.raises(reggov_api_docket_error.BadDocketID):
+            response = get_docket(API_KEY, bad_docket)
+
+
+def test_no_docket_id():
+    with requests_mock.Mocker() as mock:
+        mock.get(URL + API_KEY + "&docketID=",
+                 json='The test should yield a bad id', status_code=404)
+
+        with pytest.raises(reggov_api_docket_error.BadDocketID):
+            response = get_docket(API_KEY, '')
+
+
+def test_bad_docket_id_pattern():
+    with requests_mock.Mocker() as mock:
+        bad_docket = 'b4d' + DOCKET_ID + 'b4d'
+        mock.get(URL + API_KEY + "&docketID=" + bad_docket,
+                 json='The test should yield a bad id pattern', status_code=400)
+
+        with pytest.raises(reggov_api_docket_error.IncorrectIDPattern):
+            response = get_docket(API_KEY, bad_docket)
+
+def test_bad_API_KEY():
+    with requests_mock.Mocker() as mock:
+        mock.get(URL + 'INVALID' + "&docketID=" + DOCKET_ID,
+                 json='The test should yield a bad api key', status_code=403)
+
+        with pytest.raises(reggov_api_docket_error.IncorrectApiKey):
+            response = get_docket('INVALID', DOCKET_ID)
+
+
+def test_no_API_KEY():
+    with requests_mock.Mocker() as mock:
+        mock.get(URL + "&docketID=" + DOCKET_ID,
+                 json='The test should yield a bad api key', status_code=403)
+
+        with pytest.raises(reggov_api_docket_error.IncorrectApiKey):
+            response = get_docket('', DOCKET_ID)
+
+
+def test_maxed_API_KEY():
+    with requests_mock.Mocker() as mock:
+        mock.get(URL + API_KEY + "&docketID=" + DOCKET_ID,
+                 json='The test should yield a overused api key', status_code=429)
+
+        with pytest.raises(reggov_api_docket_error.ExceedCallLimit):
+            response = get_docket(API_KEY, DOCKET_ID)
