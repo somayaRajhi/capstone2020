5ea11339705dcda6249d58c10080fb8e10c0407a
Ben Coleman <colemanb@moravian.edu>
2020-03-06 11:28:48 -0500

    Make code pylint and pep8 compliant

--- src/c20_server/numbers.py
+++ src/c20_server/numbers.py
@@ -1,21 +1,10 @@
-"""
-Module
-"""
+
 
 class Numbers:
-    """
-    There.
-    """
     value = 100
 
     def get_value(self):
-        """
-        Hi.
-        """
         return self.value
 
     def set_value(self, value):
-        """
-        Hi.
-        """
         self.value = value
