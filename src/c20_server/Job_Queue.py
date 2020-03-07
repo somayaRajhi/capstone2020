4d310f92a5987681c06c0bd8e1c349165b6f6435
riyadgh <riyad.progs@gmail.com>
2020-03-07 15:27:27 -0500

    create Job-Queue class with add_job, get_job, and get_num_unsigned_jobs methods

---
+++ src/c20_server/Job_Queue.py
@@ -0,0 +1,30 @@
+
+"""
+implementation of Job_Queue class
+
+"""
+
+
+class JobQueue:
+    """
+    Job Queue class.
+    """
+    unsigned_jobs_list = []
+
+    def add_job(self,job_id):
+        """
+        add a new job to the unsigned_jobs_list.
+        """
+        self.unsigned_jobs_list.append(job_id)
+
+    def get_job(self):
+        """
+        Get a job from the unsigned_jobs_list, and then return it.
+        """
+        return self.unsigned_jobs_list.pop()
+
+    def get_num_unsigned_jobs(self):
+        """
+        Get the number of jobs in the unsigned_jobs_list, and then return it.
+        """
+        return len(self.unsigned_jobs_list)
