173bd962f4cc0e489f8a7713442c9b54ee4c401f
Nischal <piyanischal@gmail.com>
2020-03-10 19:29:56 -0400

    Remove test_url and test_user from test

--- tests/get_job_test.py
+++ tests/get_job_test.py
@@ -18,13 +18,5 @@ def test_get_job(client):
     assert response.data == b'{"docketId":"CMS-2014-0115"}\n'
 
 
-def test_url(client):
-    response = client.get('/job/')
-    assert response.status_code == 404
-
-
-def test_user(client):
-    response = client.get('/request/CMS')
-    assert response.data == b'{"Success":"Access Granted"}\n'
 
 
