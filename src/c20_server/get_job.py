a11f0b063c43c58cbd6798409ec1dd2c7a81ffc6
Nischal <piyanischal@gmail.com>
2020-03-10 19:18:08 -0400

    Remove docketId and username endpoints

--- src/c20_server/get_job.py
+++ src/c20_server/get_job.py
@@ -10,17 +10,5 @@ def get_job():
     }
 
 
-@app.route('/job/<docketId>')
-def get_task(docketId):
-    if len(docketId) == 0:
-        return {"Error": "URL not found"}
-    return {"docketId": docketId}
-
-
-@app.route('/request/<username>')
-def client_username_validation(username):
-    if username != 'CMS':
-        return {"Error": "Access Denied"}
-    return {"Success": "Access Granted"}
 
 
