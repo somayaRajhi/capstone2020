7a90183ed5394f5970ce786ec5d3f29f0a6e3205
student <student@ml-studentloaner5.students.moravian.edu>
2020-03-11 12:34:45 -0400

    add a new error case to check

--- src/c20_server/document_download.py
+++ src/c20_server/document_download.py
@@ -1,5 +1,5 @@
 import requests
-from c20_server import regulations_api_errors
+from c20_server import reggov_api_doc_error
 
 
 def download_document(api_key, document_id=""):
@@ -8,14 +8,16 @@ def download_document(api_key, document_id=""):
     """
     url = "https://api.data.gov:443/regulations/v3/documents.json?"
     if document_id == "":
-        data = requests.get(url + api_key + '&rpp=1')
+        response = requests.get(url + api_key + '&rpp=1')
     else:
-        data = requests.get(url + api_key + "documentId=" + document_id)
-    if data.status_code == 403:
-        raise regulations_api_errors.InvalidApiKeyException
-    if data.status_code == 429:
-        raise regulations_api_errors.RateLimitException
-    if data.status_code == 404:
-        raise regulations_api_errors.BadDocumentIDException
-    document = data.json()
+        response = requests.get(url + api_key + "documentId=" + document_id)
+    if response.status_code == 400:
+        raise reggov_api_doc_error.IncorrectIDPatternException
+    if response.status_code == 403:
+        raise reggov_api_doc_error.IncorrectApiKeyException
+    if response.status_code == 404:
+        raise reggov_api_doc_error.BadDocIDException
+    if response.status_code == 429:
+        raise reggov_api_doc_error.ExceedCallLimitException
+    document = response.json()
     return document
