249879244a49b65a8b4da0edacae56a6a2cd1528
riyadgh <riyad.progs@gmail.com>
2020-03-11 15:46:02 -0400

    fix jobs list name

--- src/c20_server/job_queue.py
+++ src/c20_server/job_queue.py
@@ -11,26 +11,26 @@ class JobQueue:
     """
     Job Queue class.
     """
-    unsigned_jobs_list = []
+    unassigned_jobs_list = []
     job = Job
 
     def add_job(self, job):
         """
         Add a new job to the unassigned_jobs_list.
         """
-        self.unsigned_jobs_list.append(job)
+        self.unassigned_jobs_list.append(job)
 
     def get_job(self):
         """
         Get a job from the unassigned_jobs_list, and then return it. If there is no jobs available
         raise an Exception.
         """
-        if not self.unsigned_jobs_list:
+        if not self.unassigned_jobs_list:
             raise job_queue_errors.NoJobsAvailableException
-        return self.unsigned_jobs_list.pop(0)
+        return self.unassigned_jobs_list.pop(0)
 
-    def get_num_unsigned_jobs(self):
+    def get_num_unassigned_jobs(self):
         """
         Get the number of jobs in the unassigned_jobs_list, and then return it.
         """
-        return len(self.unsigned_jobs_list)
+        return len(self.unassigned_jobs_list)
