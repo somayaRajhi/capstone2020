da6df037a79c662618c60e3a4b7b36454a1a79aa
student <student@ml-studentloaner5.students.moravian.edu>
2020-03-11 15:11:39 -0400

    switch to the correct endpoint

--- src/c20_server/document_download.py
+++ src/c20_server/document_download.py
@@ -6,18 +6,15 @@ def download_document(api_key, document_id=""):
     """
     downloads a file based on a url, api key and document_id (if given)
     """
-    url = "https://api.data.gov:443/regulations/v3/documents.json?"
-    if document_id == "":
-        response = requests.get(url + api_key + '&rpp=1')
-    else:
-        response = requests.get(url + api_key + "documentId=" + document_id)
-    if response.status_code == 400:
+    url = "https://api.data.gov:443/regulations/v3/document.json?"
+    data = requests.get(url + "&api_key=" + api_key + "&documentId=" + document_id)
+    if data.status_code == 400:
         raise reggov_api_doc_error.IncorrectIDPatternException
-    if response.status_code == 403:
+    if data.status_code == 403:
         raise reggov_api_doc_error.IncorrectApiKeyException
-    if response.status_code == 404:
+    if data.status_code == 404:
         raise reggov_api_doc_error.BadDocIDException
-    if response.status_code == 429:
+    if data.status_code == 429:
         raise reggov_api_doc_error.ExceedCallLimitException
-    document = response.json()
+    document = data.json()
     return document
