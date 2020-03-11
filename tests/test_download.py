dc31a98353a4a527a27a039552224cef005fde9a
student <student@ml-studentloaner5.students.moravian.edu>
2020-03-11 15:12:58 -0400

    fix the test to work with the new url as well as remove redunant tests

--- tests/test_download.py
+++ tests/test_download.py
@@ -3,61 +3,43 @@ import requests_mock
 from c20_server import document_download
 from c20_server import reggov_api_doc_error
 
-URL = "https://api.data.gov:443/regulations/v3/documents.json?"
+URL = "https://api.data.gov:443/regulations/v3/document.json?"
 DOC_ID = "EPA-HQ-OAR-2011-0028-0108"
 API_KEY = "12345"
 
 
 def test_mock_response():
     with requests_mock.Mocker() as mock:
-        mock.get(URL + "12345" + '&rpp=1', json='document received')
-        response = document_download.download_document(API_KEY)
-        assert response == 'document received'
-
-
-def test_document_id():
-    with requests_mock.Mocker() as mock:
-        mock.get(URL + API_KEY + "documentId=" + DOC_ID,
-                 json='Json response')
+        mock.get(URL, json='document received')
         response = document_download.download_document(API_KEY, DOC_ID)
-        assert response == 'Json response'
-
-
-def test_document_id_and_no_document_id():
-    with requests_mock.Mocker() as mock:
-        mock.get(URL + API_KEY + "documentId=" + DOC_ID,
-                 json='document ID response')
-        mock.get(URL + API_KEY + '&rpp=1', json='non-document ID response')
-        id_response = document_download.download_document(API_KEY, DOC_ID)
-        response = document_download.download_document(API_KEY)
-        assert response == 'non-document ID response'
-        assert id_response == 'document ID response'
+        assert response == 'document received'
 
 
 def test_incorrect_id_pattern():
     with requests_mock.Mocker() as mock:
-        mock.get(URL + API_KEY + '&rpp=1', json={'a': 'b'}, status_code=400)
+        mock.get(URL, json={'a': 'b'}, status_code=400)
         with pytest.raises(reggov_api_doc_error.IncorrectIDPatternException):
-            document_download.download_document(API_KEY)
+            bad_pattern = 'b4d' + DOC_ID + 'b4d'
+            document_download.download_document(API_KEY, bad_pattern)
 
 
 def test_incorrect_api_key():
     with requests_mock.Mocker() as mock:
-        mock.get(URL + '' + '&rpp=1', json={'a': 'b'}, status_code=403)
+        mock.get(URL, json={'a': 'b'}, status_code=403)
         with pytest.raises(reggov_api_doc_error.IncorrectApiKeyException):
-            document_download.download_document("")
+            document_download.download_document('INVALID', DOC_ID)
 
 
 def test_bad_document_id():
     with requests_mock.Mocker() as mock:
-        mock.get(URL + API_KEY + "documentId=" + DOC_ID,
-                 json={'a': 'b'}, status_code=404)
+        bad_id = DOC_ID + "-0101"
+        mock.get(URL, json={'a': 'b'}, status_code=404)
         with pytest.raises(reggov_api_doc_error.BadDocIDException):
-            document_download.download_document(API_KEY, DOC_ID)
+            document_download.download_document(API_KEY, bad_id)
 
 
 def test_exceed_call_limit():
     with requests_mock.Mocker() as mock:
-        mock.get(URL + API_KEY + '&rpp=1', json={'a': 'b'}, status_code=429)
+        mock.get(URL, json={'a': 'b'}, status_code=429)
         with pytest.raises(reggov_api_doc_error.ExceedCallLimitException):
-            document_download.download_document(API_KEY)
+            document_download.download_document(API_KEY, DOC_ID)
