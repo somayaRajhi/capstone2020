fd6528b4e523329eb40ab27df05834d0acc66f72
riyadgh <riyad.progs@gmail.com>
2020-03-11 15:43:21 -0400

    change job_add to store objects

--- src/c20_server/job_queue.py
+++ src/c20_server/job_queue.py
@@ -3,6 +3,8 @@
 implementation of Job_Queue class
 
 """
+from c20_server.job import Job
+from c20_server import job_queue_errors
 
 
 class JobQueue:
@@ -10,21 +12,25 @@ class JobQueue:
     Job Queue class.
     """
     unsigned_jobs_list = []
+    job = Job
 
-    def add_job(self, job_id):
+    def add_job(self, job):
         """
-        add a new job to the unsigned_jobs_list.
+        Add a new job to the unassigned_jobs_list.
         """
-        self.unsigned_jobs_list.append(job_id)
+        self.unsigned_jobs_list.append(job)
 
     def get_job(self):
         """
-        Get a job from the unsigned_jobs_list, and then return it.
+        Get a job from the unassigned_jobs_list, and then return it. If there is no jobs available
+        raise an Exception.
         """
-        return self.unsigned_jobs_list.pop()
+        if not self.unsigned_jobs_list:
+            raise job_queue_errors.NoJobsAvailableException
+        return self.unsigned_jobs_list.pop(0)
 
     def get_num_unsigned_jobs(self):
         """
-        Get the number of jobs in the unsigned_jobs_list, and then return it.
+        Get the number of jobs in the unassigned_jobs_list, and then return it.
         """
         return len(self.unsigned_jobs_list)
