34c8aafcc96cf975bd8121641017360339d1f6a2
kschmall <schmallk@moravian.edu>
2020-03-16 13:42:20 -0400

    Add test file for list documents file

--- src/c20_server/list_documents.py
+++ src/c20_server/list_documents.py
@@ -1,3 +1,10 @@
 '''
 List Documents from regulations.api
 '''
+
+def jformat(documents):
+    '''
+    Format String of JSON document
+    '''
+    formatted = json.dumps(documents, sort_keys=True, indent=4)
+    return formatted
---
+++ tests/test_list_documents.py
@@ -0,0 +1,12 @@
+
+import pytest
+import requests_mock
+from c20_server import list_documents
+from c20_server import reggov_api_doc_error
+
+URL = "https://api.data.gov:443/regulations/v3/document.json?"
+API_KEY = "12345"
+
+def test_mock_response():
+    response = list_documents.list_documents()
+    assert response == 10
