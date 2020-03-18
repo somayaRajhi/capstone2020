822ca97f3f43fe36b961d08862056cce2b66fa93
elijahives <ivese@moravian.edu>
2020-03-18 15:04:39 -0400

    add functions to gather extra jobs into a list

--- src/c20_server/document_download.py
+++ src/c20_server/document_download.py
@@ -1,5 +1,23 @@
 import requests
 from c20_server import reggov_api_doc_error
+import os
+from dotenv import load_dotenv, find_dotenv
+
+def extract_file_formats(document):
+    if 'fileFormats' in document:
+        file_formats = document['fileFormats']
+        return file_formats
+    return None
+
+
+def extract_attachments(document):
+    attachments = []
+    number_of_attachments  = len(document['attachments'])
+    if 'attachments' in document:
+        for i in range(0, number_of_attachments):
+            attachments += document["attachments"][i]['fileFormats']
+        return attachments
+    return None
 
 
 def download_document(api_key, document_id=""):
@@ -20,4 +38,24 @@ def download_document(api_key, document_id=""):
     if data.status_code == 429:
         raise reggov_api_doc_error.ExceedCallLimitException
     document = data.json()
-    return document
+    file_formats = extract_file_formats(document)
+    file_attachments = extract_attachments(document)
+    return document, file_formats, file_attachments
+
+
+def main():
+    load_dotenv(find_dotenv())
+    api_key = os.getenv("API_KEY")
+    doc_id = "BIS-2018-0006-47983"
+    result1, result2, result3 = download_document(api_key, doc_id)
+    print(result1)
+    print(result2)
+    print(result3)
+    
+
+if __name__ == '__main__':
+    main()
+
+
+
+
