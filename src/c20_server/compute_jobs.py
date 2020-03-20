e6b5273d6cefe666b61120ba541e983256ab8bb8
kschmall <schmallk@moravian.edu>
2020-03-20 15:02:18 -0400

    Add crd date to function

--- src/c20_server/compute_jobs.py
+++ src/c20_server/compute_jobs.py
@@ -1,7 +1,8 @@
 '''
-List Documents from regulations.api
+Compute Jobs from regulations.api
 '''
 # import json
+from datetime import date
 import requests
 from c20_client import reggov_api_doc_error
 
@@ -9,13 +10,22 @@ URL = "https://api.data.gov:443/regulations/v3/documents.json?api_key="
 RESULTS_PER_PAGE = 1000
 
 
-def compute_jobs(api_key):
+def compute_jobs(api_key, start_date):
 
-    response = requests.get(URL+api_key)
+    # Getting current date in mm/dd/yy format
+    today = date.today()
+    date_now = today.strftime("%m/%d/%y")
+
+    # Test to see if start_date is valid
+
+    crd = start_date + "-" + date_now
+
+    response = requests.get(URL+api_key+"&crd="+crd)
 
     if response.status_code == 403:
         raise reggov_api_doc_error.IncorrectApiKeyException
     if response.status_code == 429:
         raise reggov_api_doc_error.ExceedCallLimitException
+    print(response.json())
 
     return response.json()
--- tests/test_compute_jobs.py
+++ tests/test_compute_jobs.py
@@ -7,13 +7,14 @@ from c20_client import reggov_api_doc_error
 
 URL = "https://api.data.gov:443/regulations/v3/documents.json?api_key="
 API_KEY = "VALID"
+START_DATE = "01/01/20"
 
 
 def test_compute_jobs():
     with requests_mock.Mocker() as mock:
         mock.get(URL + API_KEY,
                  json='The test is successful')
-        response = compute_jobs(API_KEY)
+        response = compute_jobs(API_KEY, START_DATE)
 
         assert response == 'The test is successful'
 
@@ -24,7 +25,7 @@ def test_bad_api_key():
                  json='The test yields a bad api key', status_code=403)
 
         with pytest.raises(reggov_api_doc_error.IncorrectApiKeyException):
-            compute_jobs('INVALID')
+            compute_jobs('INVALID', START_DATE)
 
 
 def test_overused_api_key():
@@ -33,4 +34,4 @@ def test_overused_api_key():
                  json='The test yields a overused api key', status_code=429)
 
         with pytest.raises(reggov_api_doc_error.ExceedCallLimitException):
-            compute_jobs(API_KEY)
+            compute_jobs(API_KEY, START_DATE)
