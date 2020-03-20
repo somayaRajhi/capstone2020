95c595e9390666d2a6a9d60a3c7e3e9548f06bed
riyadgh <riyad.progs@gmail.com>
2020-03-20 13:42:45 -0400

    Implement the In-Progress methods, and some basic tests that check all the ideas

--- src/c20_server/in_progress.py
+++ src/c20_server/in_progress.py
@@ -1,3 +1,20 @@
 """
 Implementation of In-Progress class
 """
+from c20_server import job_queue_errors
+
+
+class InProgress:
+
+    assigned_jobs_list = []
+
+    def assign(self, job, user):
+        self.assigned_jobs_list.append((job, user))
+
+    def unassign(self, job, user):
+        if (job, user) not in self.assigned_jobs_list:
+            raise job_queue_errors.UnassignInvalidDataException
+        self.assigned_jobs_list.remove((job, user))
+
+    def get_num_assigned_jobs(self):
+        return len(self.assigned_jobs_list)
--- tests/test_in_progress.py
+++ tests/test_in_progress.py
@@ -2,3 +2,40 @@
 """
 Test class for In-Progress Class.
 """
+import pytest
+from c20_server import job_queue_errors
+from c20_server.job import Job
+from c20_server.user import User
+from c20_server.in_progress import InProgress
+
+
+JOB1 = Job(123)
+USER1 = User(1)
+
+
+def test_no_assigned_jobs():
+    assert InProgress().get_num_assigned_jobs() == 0
+
+
+def test_assign_one_job():
+    assert InProgress().get_num_assigned_jobs() == 0
+    InProgress().assign(JOB1, USER1)
+    assert InProgress().get_num_assigned_jobs() == 1
+
+
+def test_unassign_invalid_job():
+    invalid_job = Job(222)
+    with pytest.raises(job_queue_errors.UnassignInvalidDataException):
+        InProgress().unassign(invalid_job, USER1)
+
+
+def test_unassign_invalid_user():
+    invalid_user = User(66)
+    with pytest.raises(job_queue_errors.UnassignInvalidDataException):
+        InProgress().unassign(JOB1, invalid_user)
+
+
+def test_unassign_one_job():
+    assert InProgress().get_num_assigned_jobs() == 1
+    InProgress().unassign(JOB1, USER1)
+    assert InProgress().get_num_assigned_jobs() == 0
