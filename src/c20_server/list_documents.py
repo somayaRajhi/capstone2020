ec34a985d1c6a566cc7ed34df313a649962384bc
kschmall <schmallk@moravian.edu>
2020-03-20 10:53:17 -0400

    Add basic tests for simple response from list_documents

--- src/c20_server/list_documents.py
+++ src/c20_server/list_documents.py
@@ -2,8 +2,23 @@
 List Documents from regulations.api
 '''
 # import json
-# import requests
+import requests
 # from c20_server import reggov_api_doc_error
 
-def list_documents():
-    return 20
+URL = "https://api.data.gov:443/regulations/v3/document.json?api_key="
+
+
+def list_documents(api_key):
+
+    response = requests.get(URL+api_key)
+    return response.json()
+
+    # if response.status_code == 403:
+    #     raise reggov_api_doc_error.IncorrectApiKeyException
+    # if response.status_code == 404:
+    #     raise reggov_api_doc_error.BadDocIDException
+    # if response.status_code == 400:
+    #     raise reggov_api_doc_error.IncorrectIDPatternException
+    # if response.status_code == 429:
+    #     raise reggov_api_doc_error.ExceedCallLimitException
+
--- tests/test_list_documents.py
+++ tests/test_list_documents.py
@@ -1,12 +1,20 @@
 
 import pytest
-# import requests_mock
-from c20_server import list_documents
+import requests_mock
+from c20_server.list_documents import list_documents
 # from c20_server import reggov_api_doc_error
 
-# URL = "https://api.data.gov:443/regulations/v3/document.json?"
-# API_KEY = "12345"
+URL = "https://api.data.gov:443/regulations/v3/document.json?api_key="
+API_KEY = "VALID"
 
 def test_list_documents():
-    response = list_documents()
-    assert response == 20
+    with requests_mock.Mocker() as mock:
+        mock.get(URL + API_KEY,
+                 json='The test is successful')
+        response = list_documents(API_KEY)
+
+        assert response == 'The test is successful'
+
+def test_bad_api_key():
+    response = "Bad API Key"
+    assert response == "Bad API Key"
