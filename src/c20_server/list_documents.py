7b66f062ad49ec2997ca5205c9377516d14f8ebe
kschmall <schmallk@moravian.edu>
2020-03-20 11:19:13 -0400

    Add tests for incorrect or overused API key

--- src/c20_server/list_documents.py
+++ src/c20_server/list_documents.py
@@ -3,7 +3,7 @@ List Documents from regulations.api
 '''
 # import json
 import requests
-# from c20_server import reggov_api_doc_error
+from c20_server import reggov_api_doc_error
 
 URL = "https://api.data.gov:443/regulations/v3/document.json?api_key="
 
@@ -11,13 +11,14 @@ URL = "https://api.data.gov:443/regulations/v3/document.json?api_key="
 def list_documents(api_key):
 
     response = requests.get(URL+api_key)
-    return response.json()
 
-    # if response.status_code == 403:
-    #     raise reggov_api_doc_error.IncorrectApiKeyException
+    if response.status_code == 403:
+        raise reggov_api_doc_error.IncorrectApiKeyException
     # if response.status_code == 404:
     #     raise reggov_api_doc_error.BadDocIDException
     # if response.status_code == 400:
     #     raise reggov_api_doc_error.IncorrectIDPatternException
-    # if response.status_code == 429:
-    #     raise reggov_api_doc_error.ExceedCallLimitException
+    if response.status_code == 429:
+        raise reggov_api_doc_error.ExceedCallLimitException
+
+    return response.json()
--- tests/test_list_documents.py
+++ tests/test_list_documents.py
@@ -1,8 +1,8 @@
 
-# import pytest
+import pytest
 import requests_mock
 from c20_server.list_documents import list_documents
-# from c20_server import reggov_api_doc_error
+from c20_server import reggov_api_doc_error
 
 URL = "https://api.data.gov:443/regulations/v3/document.json?api_key="
 API_KEY = "VALID"
@@ -18,5 +18,18 @@ def test_list_documents():
 
 
 def test_bad_api_key():
-    response = "Bad API Key"
-    assert response == "Bad API Key"
+    with requests_mock.Mocker() as mock:
+        mock.get(URL + 'INVALID',
+                 json='The test yields a bad api key', status_code=403)
+
+        with pytest.raises(reggov_api_doc_error.IncorrectApiKeyException):
+            list_documents('INVALID')
+
+
+def test_overused_api_key():
+    with requests_mock.Mocker() as mock:
+        mock.get(URL + API_KEY,
+                 json='The test yields a overused api key', status_code=429)
+
+        with pytest.raises(reggov_api_doc_error.ExceedCallLimitException):
+            list_documents(API_KEY)
