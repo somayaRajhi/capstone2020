751885f0a87e123ea21e66198a7b665c2f3114ca
Somaya Rajhi <stsar04@moravian.edu>
2020-03-20 15:47:23 -0400

    do_job function implementions

--- src/c20_client/c20_client.py
+++ src/c20_client/c20_client.py
@@ -1,7 +1,33 @@
+import json
 import requests
 
 
+
 def do_job():
-    requests.get('http://capstone.cs.moravian.edu')
-    requests.get('https://api.data.gov')
+    try:
+        job = requests.get('http://capstone.cs.moravian.edu/get_job')
+        job=job.json()
+        url=job['url']
+
+        job_type = job['job_type']
+
+    except Exception as e:
+        print(e)
+
+    if job_type == 'documents':
+        requests.get(url)
+
+    elif job_type=='docket':
+        requests.get(url)
+
+    elif job_type=='document':
+        requests.get(url)
+
+    elif job_type=='download':
+        requests.get(url)
+
+    elif job_type == 'none':
+        requests.get(url)
+
     requests.post('http://capstone.cs.moravian.edu')
+
