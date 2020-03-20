37a3673bf44c221d6b8ae7aa668821ec66680469
Somaya Rajhi <stsar04@moravian.edu>
2020-03-20 15:49:20 -0400

    two tests for do_job function

--- tests/c20_client/test_client.py
+++ tests/c20_client/test_client.py
@@ -1,12 +1,12 @@
-
 import requests_mock
 from c20_client.c20_client import do_job
 
 
 def test_client_calls_server_regulations_server():
     with requests_mock.Mocker() as mock:
-        mock.get('http://capstone.cs.moravian.edu', json={})
-        mock.get('https://api.data.gov', json={})
+        mock.get('http://capstone.cs.moravian.edu/get_job', json={'job_type': 'document'
+                    ,"url":"https://api.data.gov/json?documentid=abc"})
+        mock.get('https://api.data.gov/json?documentid=abc', json = {})
         mock.post('http://capstone.cs.moravian.edu', json={})
         do_job()
         history = mock.request_history
@@ -15,3 +15,17 @@ def test_client_calls_server_regulations_server():
         assert 'capstone' in history[0].url
         assert 'api.data.gov' in history[1].url
         assert 'capstone' in history[2].url
+
+def test_client_calls_decument_endpoint_for_document_job():
+            with requests_mock.Mocker() as mock:
+                mock.get('http://capstone.cs.moravian.edu/get_job', json={'job_type': 'document'
+                    ,"url":"https://api.data.gov/json?documentid=abc"})
+                mock.get('https://api.data.gov', json={})
+                mock.post('http://capstone.cs.moravian.edu', json={})
+                do_job()
+                history = mock.request_history
+
+                assert len(history) == 3
+                assert 'capstone' in history[0].url
+                assert 'api.data.gov' in history[1].url
+                assert 'capstone' in history[2].url
