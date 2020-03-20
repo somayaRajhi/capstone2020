d17b244b386cc1a5bbfc82afaf111e9a0c483c98
kschmall <schmallk@moravian.edu>
2020-03-20 13:01:10 -0400

    Fix line too long error

--- tests/test_list_documents.py
+++ tests/test_list_documents.py
@@ -1,7 +1,11 @@
 
 import pytest
 import requests_mock
-from c20_server.list_documents import list_documents, list_docket_ids, list_document_ids
+from c20_server.list_documents import (
+    list_documents,
+    list_docket_ids,
+    list_document_ids,
+)
 from c20_server import reggov_api_doc_error
 
 URL = "https://api.data.gov:443/regulations/v3/documents.json?api_key="
