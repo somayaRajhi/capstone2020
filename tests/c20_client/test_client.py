fdda43ae6265f34d8c1447a3fec5952a5a812765
Somaya Rajhi <stsar04@moravian.edu>
2020-03-21 13:52:00 -0400

    fixed Static Analysis testing

--- tests/c20_client/test_client.py
+++ tests/c20_client/test_client.py
@@ -4,10 +4,13 @@ from c20_client.c20_client import do_job
 
 def test_client_calls_decuments_endpoint_for_documents_job():
     with requests_mock.Mocker() as mock:
-        mock.get('http://capstone.cs.moravian.edu/get_job', json={'job_type': 'documents'
-                    ,"url":"https://api.data.gov/json?documents=abc"})
-        mock.get('https://api.data.gov/json?documents=abc', json = {'documents':'abc'})
-        mock.post('http://capstone.cs.moravian.edu', json={})
+        mock.get('http://capstone.cs.moravian.edu/get_job',
+                 json={'job_type': 'documents',
+                "url":"https://api.data.gov/json?documents=abc"})
+        mock.get('https://api.data.gov/json?documents=abc',
+                 json = {'documents':'abc'})
+        mock.post('http://capstone.cs.moravian.edu',
+                  json={})
         do_job()
         history = mock.request_history
 
@@ -18,10 +21,13 @@ def test_client_calls_decuments_endpoint_for_documents_job():
 
 def test_client_calls_decument_endpoint_for_document_job():
             with requests_mock.Mocker() as mock:
-                mock.get('http://capstone.cs.moravian.edu/get_job', json={'job_type': 'document'
-                    ,"url":"https://api.data.gov/json?documentid=abc"})
-                mock.get('https://api.data.gov/json?documentid=abc', json={})
-                mock.post('http://capstone.cs.moravian.edu', json={})
+                mock.get('http://capstone.cs.moravian.edu/get_job',
+                    json={'job_type': 'document',
+                    "url":"https://api.data.gov/json?documentid=abc"})
+                mock.get('https://api.data.gov/json?documentid=abc',
+                         json={})
+                mock.post('http://capstone.cs.moravian.edu',
+                          json={})
                 do_job()
                 history = mock.request_history
 
@@ -32,10 +38,13 @@ def test_client_calls_decument_endpoint_for_document_job():
 
 def test_client_calls_docket_endpoint_for_docket_job():
             with requests_mock.Mocker() as mock:
-                mock.get('http://capstone.cs.moravian.edu/get_job', json={'job_type': 'docket'
-                            , "url": "https://api.data.gov/json?docketid=abc"})
-                mock.get('https://api.data.gov/json?docketid=abc', json={})
-                mock.post('http://capstone.cs.moravian.edu', json={})
+                mock.get('http://capstone.cs.moravian.edu/get_job',
+                         json={'job_type': 'docket',
+                        "url": "https://api.data.gov/json?docketid=abc"})
+                mock.get('https://api.data.gov/json?docketid=abc',
+                         json={})
+                mock.post('http://capstone.cs.moravian.edu',
+                          json={})
                 do_job()
                 history = mock.request_history
 
@@ -46,10 +55,13 @@ def test_client_calls_docket_endpoint_for_docket_job():
 
 def test_client_calls_download_endpoint_for_download_job():
             with requests_mock.Mocker() as mock:
-                mock.get('http://capstone.cs.moravian.edu/get_job', json={'job_type': 'download'
-                        , "url": "https://api.data.gov/json?download=abc"})
-                mock.get('https://api.data.gov/json?download=abc', json={})
-                mock.post('http://capstone.cs.moravian.edu', json={})
+                mock.get('http://capstone.cs.moravian.edu/get_job',
+                         json={'job_type': 'download',
+                        "url": "https://api.data.gov/json?download=abc"})
+                mock.get('https://api.data.gov/json?download=abc',
+                         json={})
+                mock.post('http://capstone.cs.moravian.edu',
+                          json={})
                 do_job()
                 history = mock.request_history
 
@@ -60,10 +72,13 @@ def test_client_calls_download_endpoint_for_download_job():
 
 def test_client_got_bad_job_from_server():
             with requests_mock.Mocker() as mock:
-                mock.get('http://capstone.cs.moravian.edu/get_job', json={'job_type': 'none'
-                            , "url": "https://api.data.gov/json?"})
-                mock.get('https://api.data.gov/json?', json={})
-                mock.post('http://capstone.cs.moravian.edu', json={})
+                mock.get('http://capstone.cs.moravian.edu/get_job',
+                         json={'job_type': 'none',
+                        "url": "https://api.data.gov/json?"})
+                mock.get('https://api.data.gov/json?',
+                         json={})
+                mock.post('http://capstone.cs.moravian.edu',
+                          json={})
                 do_job()
                 history = mock.request_history
 
