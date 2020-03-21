10fbd2b638b85875bd4f8cd0ff553f5cbc99066f
anthonyestephan <estephana02@moravian.edu>
2020-03-21 15:30:31 -0400

    fix pylint errors

--- src/c20_client/c20_client.py
+++ src/c20_client/c20_client.py
@@ -1,24 +1,31 @@
+"""
+Gets a job from the server and handles the job based on the type of job
+"""
 import requests
 
 
 def do_job():
+    """
+    Gets a job from the server and handles the job based on the type of job
+    """
     try:
-        job=requests.get('http://capstone.cs.moravian.edu/get_job')
-        job=job.json()
-        url=job['url']
-        job_type=job['job_type']
+        job = requests.get('http://capstone.cs.moravian.edu/get_job')
+        job = job.json()
+        url = job['url']
+        job_type = job['job_type']
 
-    except Exception as e:
-        print(e)
+    except Exception as exception:
+        print(exception)
 
-    if job_type=='documents':
+    if job_type == 'documents':
         requests.get(url)
-    elif job_type=='docket':
+    elif job_type == 'docket':
         requests.get(url)
-    elif job_type=='document':
+    elif job_type == 'document':
         requests.get(url)
-    elif job_type=='download':
+    elif job_type == 'download':
         requests.get(url)
-    elif job_type=='none':
+    elif job_type == 'none':
         requests.get(url)
+
     requests.post('http://capstone.cs.moravian.edu')
--- tests/c20_client/test_client.py
+++ tests/c20_client/test_client.py
@@ -6,9 +6,9 @@ def test_client_calls_decuments_endpoint_for_documents_job():
     with requests_mock.Mocker() as mock:
         mock.get('http://capstone.cs.moravian.edu/get_job',
                  json={'job_type': 'documents',
-                "url":"https://api.data.gov/json?documents=abc"})
+                       "url": "https://api.data.gov/json?documents=abc"})
         mock.get('https://api.data.gov/json?documents=abc',
-                 json = {'documents':'abc'})
+                 json={'documents': 'abc'})
         mock.post('http://capstone.cs.moravian.edu',
                   json={})
         do_job()
@@ -19,70 +19,74 @@ def test_client_calls_decuments_endpoint_for_documents_job():
         assert 'api.data.gov' in history[1].url
         assert 'capstone' in history[2].url
 
-def test_client_calls_decument_endpoint_for_document_job():
-            with requests_mock.Mocker() as mock:
-                mock.get('http://capstone.cs.moravian.edu/get_job',
-                    json={'job_type': 'document',
-                    "url":"https://api.data.gov/json?documentid=abc"})
-                mock.get('https://api.data.gov/json?documentid=abc',
-                         json={})
-                mock.post('http://capstone.cs.moravian.edu',
-                          json={})
-                do_job()
-                history = mock.request_history
 
-                assert len(history) == 3
-                assert 'capstone' in history[0].url
-                assert 'api.data.gov' in history[1].url
-                assert 'capstone' in history[2].url
+def test_client_calls_decument_endpoint_for_document_job():
+    with requests_mock.Mocker() as mock:
+        mock.get('http://capstone.cs.moravian.edu/get_job',
+                 json={'job_type': 'document',
+                       "url": "https://api.data.gov/json?documentid=abc"})
+        mock.get('https://api.data.gov/json?documentid=abc',
+                 json={})
+        mock.post('http://capstone.cs.moravian.edu',
+                  json={})
+        do_job()
+        history = mock.request_history
+
+        assert len(history) == 3
+        assert 'capstone' in history[0].url
+        assert 'api.data.gov' in history[1].url
+        assert 'capstone' in history[2].url
+
 
 def test_client_calls_docket_endpoint_for_docket_job():
-            with requests_mock.Mocker() as mock:
-                mock.get('http://capstone.cs.moravian.edu/get_job',
-                         json={'job_type': 'docket',
-                        "url": "https://api.data.gov/json?docketid=abc"})
-                mock.get('https://api.data.gov/json?docketid=abc',
-                         json={})
-                mock.post('http://capstone.cs.moravian.edu',
-                          json={})
-                do_job()
-                history = mock.request_history
+    with requests_mock.Mocker() as mock:
+        mock.get('http://capstone.cs.moravian.edu/get_job',
+                 json={'job_type': 'docket',
+                       "url": "https://api.data.gov/json?docketid=abc"})
+        mock.get('https://api.data.gov/json?docketid=abc',
+                 json={})
+        mock.post('http://capstone.cs.moravian.edu',
+                  json={})
+        do_job()
+        history = mock.request_history
 
-                assert len(history) == 3
-                assert 'capstone' in history[0].url
-                assert 'api.data.gov' in history[1].url
-                assert 'capstone' in history[2].url
+        assert len(history) == 3
+        assert 'capstone' in history[0].url
+        assert 'api.data.gov' in history[1].url
+        assert 'capstone' in history[2].url
+
 
 def test_client_calls_download_endpoint_for_download_job():
-            with requests_mock.Mocker() as mock:
-                mock.get('http://capstone.cs.moravian.edu/get_job',
-                         json={'job_type': 'download',
-                        "url": "https://api.data.gov/json?download=abc"})
-                mock.get('https://api.data.gov/json?download=abc',
-                         json={})
-                mock.post('http://capstone.cs.moravian.edu',
-                          json={})
-                do_job()
-                history = mock.request_history
+    with requests_mock.Mocker() as mock:
+        mock.get('http://capstone.cs.moravian.edu/get_job',
+                 json={'job_type': 'download',
+                       "url": "https://api.data.gov/json?download=abc"})
+        mock.get('https://api.data.gov/json?download=abc',
+                 json={})
+        mock.post('http://capstone.cs.moravian.edu',
+                  json={})
+        do_job()
+        history = mock.request_history
 
-                assert len(history) == 3
-                assert 'capstone' in history[0].url
-                assert 'api.data.gov' in history[1].url
-                assert 'capstone' in history[2].url
+        assert len(history) == 3
+        assert 'capstone' in history[0].url
+        assert 'api.data.gov' in history[1].url
+        assert 'capstone' in history[2].url
+
 
 def test_client_got_bad_job_from_server():
-            with requests_mock.Mocker() as mock:
-                mock.get('http://capstone.cs.moravian.edu/get_job',
-                         json={'job_type': 'none',
-                        "url": "https://api.data.gov/json?"})
-                mock.get('https://api.data.gov/json?',
-                         json={})
-                mock.post('http://capstone.cs.moravian.edu',
-                          json={})
-                do_job()
-                history = mock.request_history
+    with requests_mock.Mocker() as mock:
+        mock.get('http://capstone.cs.moravian.edu/get_job',
+                 json={'job_type': 'none',
+                       "url": "https://api.data.gov/json?"})
+        mock.get('https://api.data.gov/json?',
+                 json={})
+        mock.post('http://capstone.cs.moravian.edu',
+                  json={})
+        do_job()
+        history = mock.request_history
 
-                assert len(history) == 3
-                assert 'capstone' in history[0].url
-                assert 'api.data.gov' in history[1].url
-                assert 'capstone' in history[2].url
+        assert len(history) == 3
+        assert 'capstone' in history[0].url
+        assert 'api.data.gov' in history[1].url
+        assert 'capstone' in history[2].url
