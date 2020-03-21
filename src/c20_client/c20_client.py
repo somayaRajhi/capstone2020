615fd5a302ab71d8567419449606c85fb3d94ec4
Somaya Rajhi <stsar04@moravian.edu>
2020-03-21 13:52:58 -0400

    fixed Static Analysis testing

--- src/c20_client/c20_client.py
+++ src/c20_client/c20_client.py
@@ -1,33 +1,21 @@
-import json
+
 import requests
-
-
-
 def do_job():
     try:
         job = requests.get('http://capstone.cs.moravian.edu/get_job')
         job=job.json()
         url=job['url']
-
         job_type = job['job_type']
-
     except Exception as e:
         print(e)
-
     if job_type == 'documents':
         requests.get(url)
-
     elif job_type=='docket':
         requests.get(url)
-
     elif job_type=='document':
         requests.get(url)
-
     elif job_type=='download':
         requests.get(url)
-
     elif job_type == 'none':
         requests.get(url)
-
     requests.post('http://capstone.cs.moravian.edu')
-
