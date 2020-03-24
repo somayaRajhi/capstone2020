3c9ce5a1bba528b2983283700083cd1422322ded
riyadgh <riyad.progs@gmail.com>
2020-03-24 13:27:12 -0400

    Refactor InProgress tests to deal with the assigned jobs dict

--- tests/test_in_progress.py
+++ tests/test_in_progress.py
@@ -9,11 +9,6 @@ from c20_server.user import User
 from c20_server.in_progress import InProgress
 
 
-JOB1 = Job(123)
-USER1 = User(1)
-IN_PROGRESS_JOBS = InProgress()
-
-
 @pytest.fixture(name='in_progress')
 def make_in_progress():
     return InProgress()
@@ -24,33 +19,25 @@ def test_new_instance_empty(in_progress):
 
 
 def test_when_job_assigned_it_is_added_to_assigned_jobs(in_progress):
-    in_progress.assign(Job(1), User(100))
+    in_progress.assign(Job(1), User(100).user_id)
     assert in_progress.get_num_assigned_jobs() == 1
 
 
-def test_no_assigned_jobs():
-    assert IN_PROGRESS_JOBS.get_num_assigned_jobs() == 0
+def test_when_job_unassigned_it_is_removed_from_assigned_jobs(in_progress):
+    in_progress.assign(Job(1), User(100).user_id)
+    assert in_progress.get_num_assigned_jobs() == 1
+    in_progress.unassign(User(100).user_id)
+    assert in_progress.get_num_assigned_jobs() == 0
 
 
-def test_assign_one_job():
-    assert IN_PROGRESS_JOBS.get_num_assigned_jobs() == 0
-    IN_PROGRESS_JOBS.assign(JOB1, USER1)
-    assert IN_PROGRESS_JOBS.get_num_assigned_jobs() == 1
-
-
-def test_unassign_invalid_job():
-    invalid_job = Job(222)
+def test_unassign_job_with_invalid_user(in_progress):
+    assert in_progress.get_num_assigned_jobs() == 0
+    in_progress.assign(Job(1), User(100).user_id)
+    invalid_user = User(20)
     with pytest.raises(job_queue_errors.UnassignInvalidDataException):
-        IN_PROGRESS_JOBS.unassign(invalid_job, USER1)
+        in_progress.unassign(invalid_user.user_id)
 
 
-def test_unassign_invalid_user():
-    invalid_user = User(66)
+def test_unassign_invalid_job(in_progress):
     with pytest.raises(job_queue_errors.UnassignInvalidDataException):
-        IN_PROGRESS_JOBS.unassign(JOB1, invalid_user)
-
-
-def test_unassign_one_job():
-    assert IN_PROGRESS_JOBS.get_num_assigned_jobs() == 1
-    IN_PROGRESS_JOBS.unassign(JOB1, USER1)
-    assert IN_PROGRESS_JOBS.get_num_assigned_jobs() == 0
+        in_progress.unassign(User(100).user_id)
