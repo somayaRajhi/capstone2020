365101ad0c0465c5331bf60d601c064b1d2ca630
riyadgh <riyad.progs@gmail.com>
2020-04-02 17:16:57 -0400

    Add a simple test that test the use of DocumentsJob as one job

--- tests/test_job_queue.py
+++ tests/test_job_queue.py
@@ -4,13 +4,18 @@ Test class for Job-Queue Class.
 """
 import pytest
 from c20_server.job_queue import JobQueue
-from c20_server.job import Job
+from c20_server.job import Job, DocumentsJob
 from c20_server import job_queue_errors
 
 
 @pytest.fixture(name='job_queue')
 def make_job_queue():
     return JobQueue()
+
+
+@pytest.fixture(name='documents_job')
+def make_first_documents_job():
+    return DocumentsJob('job01', 1000, '2020-1-28', '2020-5-6')
 
 
 def test_one_job_added_is_returned_by_get(job_queue):
@@ -39,3 +44,17 @@ def test_add_two_jobs_then_remove_them(job_queue):
 def test_get_one_job_from_empty_list(job_queue):
     with pytest.raises(job_queue_errors.NoJobsAvailableException):
         job_queue.get_job()
+
+
+def test_one_documents_job_added_is_returned_by_get(job_queue, documents_job):
+
+    job_queue.add_job(documents_job)
+    assert job_queue.get_num_unassigned_jobs() == 1
+
+    requested_job = job_queue.get_job()
+    assert job_queue.get_num_unassigned_jobs() == 0
+
+    assert requested_job.job_id == 'job01'
+    assert requested_job.page_offset == 1000
+    assert requested_job.start_date == '2020-1-28'
+    assert requested_job.end_date == '2020-5-6'
