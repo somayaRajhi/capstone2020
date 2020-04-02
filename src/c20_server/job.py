2b0420142cea9b48ec9902469e5d457eadbc34dc
riyadgh <riyad.progs@gmail.com>
2020-04-02 17:13:35 -0400

    Create and add to the DocumentsJob class

--- src/c20_server/job.py
+++ src/c20_server/job.py
@@ -3,3 +3,12 @@ class Job:
 
     def __init__(self, job_id):
         self.job_id = job_id
+
+
+class DocumentsJob(Job):
+
+    def __init__(self, job_id, page_offset, start_date, end_date):
+        super().__init__(job_id)
+        self.page_offset = page_offset
+        self.start_date = start_date
+        self.end_date = end_date
