4288b8b15fe6a26736170743829a26ebc236cc79
riyadgh <riyad.progs@gmail.com>
2020-04-03 14:06:00 -0400

    Change the DocumentsJob implementation to use a namedtuple

--- src/c20_server/job.py
+++ src/c20_server/job.py
@@ -1,3 +1,5 @@
+from collections import namedtuple
+
 
 class Job:
 
@@ -5,10 +7,10 @@ class Job:
         self.job_id = job_id
 
 
-class DocumentsJob(Job):
-
-    def __init__(self, job_id, page_offset, start_date, end_date):
-        super().__init__(job_id)
-        self.page_offset = page_offset
-        self.start_date = start_date
-        self.end_date = end_date
+class DocumentsJob(namedtuple('DocumentsJob',
+                              ['job_id',
+                               'page_offset',
+                               'start_date',
+                               'end_date']
+                              )):
+    pass
