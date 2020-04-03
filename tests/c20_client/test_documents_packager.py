804c925ae70eab113ab952f7e355bfca889b9fb0
Tim <fauxt@moravian.edu>
2020-04-03 14:26:17 -0400

    fix some pylint

--- tests/c20_client/test_documents_packager.py
+++ tests/c20_client/test_documents_packager.py
@@ -1,5 +1,5 @@
-from c20_client import documents_packager
 import pytest
+from c20_client import documents_packager
 
 TEST_JSON = {
     'documents': [{
@@ -38,7 +38,7 @@ def multiple_documents():
 
 
 def test_get_documents(single_documents):
-    assert single_documents[0] == {
+    assert single_documents == [{
         'client_id': 1,
         'data': [{
             'folder_name': 'test/docket-number-3/document-number-15/',
@@ -50,7 +50,7 @@ def test_get_documents(single_documents):
                 'documentId': 'document-number-15'
             }
         }]
-    }
+    }]
 
 
 def test_client_id(single_documents):
@@ -58,28 +58,33 @@ def test_client_id(single_documents):
 
 
 def test_folder_name(single_documents):
-    assert single_documents[0]['data'][0]['folder_name'] ==\
+    assert single_documents[0]['data'][0]['folder_name'] == \
         'test/docket-number-3/document-number-15/'
 
 
 def test_file_name(single_documents):
-    assert single_documents[0]['data'][0]['file_name'] == 'basic_documents.json'
+    assert single_documents[0]['data'][0]['file_name'] == \
+           'basic_documents.json'
 
 
 def test_agency_acronym(single_documents):
-    assert single_documents[0]['data'][0]['data']['agencyAcronym'] == 'test'
+    assert single_documents[0]['data'][0]['data']['agencyAcronym'] == \
+           'test'
 
 
 def test_data(single_documents):
-    assert single_documents[0]['data'][0]['data']['data'] == 'test Documents'
+    assert single_documents[0]['data'][0]['data']['data'] == \
+           'test Documents'
 
 
 def test_docket_id(single_documents):
-    assert single_documents[0]['data'][0]['data']['docketId'] == 'docket-number-3'
+    assert single_documents[0]['data'][0]['data']['docketId'] == \
+           'docket-number-3'
 
 
 def test_document_id(single_documents):
-    assert single_documents[0]['data'][0]['data']['documentId'] == 'document-number-15'
+    assert single_documents[0]['data'][0]['data']['documentId'] == \
+           'document-number-15'
 
 
 def test_get_many_documents(multiple_documents):
@@ -104,32 +109,42 @@ def test_many_client_id(multiple_documents):
 
 
 def test_many_folder_name(multiple_documents):
-    assert multiple_documents[0]['data'][0]['folder_name'] ==\
+    assert multiple_documents[0]['data'][0]['folder_name'] == \
         'test/docket-number-3/document-number-15/'
     assert multiple_documents[1]['data'][0]['folder_name'] == \
-           'test/docket-number-4/document-number-16/'
+        'test/docket-number-4/document-number-16/'
 
 
 def test_many_file_name(multiple_documents):
-    assert multiple_documents[0]['data'][0]['file_name'] == 'basic_documents.json'
-    assert multiple_documents[1]['data'][0]['file_name'] == 'basic_documents.json'
+    assert multiple_documents[0]['data'][0]['file_name'] == \
+        'basic_documents.json'
+    assert multiple_documents[1]['data'][0]['file_name'] == \
+        'basic_documents.json'
 
 
 def test_many_agency_acronym(multiple_documents):
-    assert multiple_documents[0]['data'][0]['data']['agencyAcronym'] == 'test'
-    assert multiple_documents[1]['data'][0]['data']['agencyAcronym'] == 'test'
+    assert multiple_documents[0]['data'][0]['data']['agencyAcronym'] == \
+        'test'
+    assert multiple_documents[1]['data'][0]['data']['agencyAcronym'] == \
+        'test'
 
 
 def test_many_data(multiple_documents):
-    assert multiple_documents[0]['data'][0]['data']['data'] == 'test Documents'
-    assert multiple_documents[1]['data'][0]['data']['data'] == 'test Documents 2'
+    assert multiple_documents[0]['data'][0]['data']['data'] == \
+        'test Documents'
+    assert multiple_documents[1]['data'][0]['data']['data'] == \
+        'test Documents 2'
 
 
 def test_many_docket_id(multiple_documents):
-    assert multiple_documents[0]['data'][0]['data']['docketId'] == 'docket-number-3'
-    assert multiple_documents[1]['data'][0]['data']['docketId'] == 'docket-number-4'
+    assert multiple_documents[0]['data'][0]['data']['docketId'] == \
+        'docket-number-3'
+    assert multiple_documents[1]['data'][0]['data']['docketId'] == \
+        'docket-number-4'
 
 
 def test_many_document_id(multiple_documents):
-    assert multiple_documents[0]['data'][0]['data']['documentId'] == 'document-number-15'
-    assert multiple_documents[1]['data'][0]['data']['documentId'] == 'document-number-16'
+    assert multiple_documents[0]['data'][0]['data']['documentId'] == \
+        'document-number-15'
+    assert multiple_documents[1]['data'][0]['data']['documentId'] == \
+        'document-number-16'
