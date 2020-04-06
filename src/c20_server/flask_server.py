5472723f4c97c9a624069866b3c9c7ab9d472bcf
Caelin Finn McCool <mccoolc@moravian.edu>
2020-04-06 14:11:59 -0400

    Add get_job functionality

--- src/c20_server/flask_server.py
+++ src/c20_server/flask_server.py
@@ -3,25 +3,27 @@ from c20_server.job_manager import JobManager
 from c20_server.user import User
 
 
-APP = Flask(__name__)
-
-
-@APP.route('/get_job')
-def get_job():
+def create_app():
+    app = Flask(__name__)
     job_manager = JobManager()
     job = {"end_date": "04-01-2020", "job_id": "1", "page_offset": "0", "start_date": "04-01-2020"}
+    job2 = {"end_date": "04-01-2020", "job_id": "2", "page_offset": "0", "start_date": "04-01-2020"}
     job_manager.add_job(job)
-    requested_job = job_manager.request_job(User(100))
-    # Translate Job
-    return requested_job
+    job_manager.add_job(job2)
 
+    @app.route('/get_job')
+    def get_job():
+        requested_job = job_manager.request_job(User(100))
+        # Translate Job
+        return requested_job
 
-@APP.route('/return_result', methods=['POST'])
-def return_result():
-    # Get result from client
-    # Translate result
-    # Return jobs to Job Manager
-    json_data = request.json
-    if json_data is None:
-        return{}, 400
-    return {}, 200
+    @app.route('/return_result', methods=['POST'])
+    def return_result():
+        # Get result from client
+        # Translate result
+        # Return jobs to Job Manager
+        json_data = request.json
+        if json_data is None:
+            return{}, 400
+        return {}, 200
+    return app
--- tests/test_flask_server.py
+++ tests/test_flask_server.py
@@ -1,22 +1,45 @@
 import json
 import pytest
-from c20_server.flask_server import APP
+from c20_server.flask_server import create_app
 
 
 @pytest.fixture(name='client')
 def client_fixture():
-    APP.config['TESTING'] = True
-    return APP.test_client()
+    app = create_app()
+    app.config['TESTING'] = True
+    return app.test_client()
 
 
 def test_get_job(client):
     response = client.get('/get_job')
     assert response.status_code == 200
     assert response.data == b'{"end_date":"04-01-2020","job_id":"1","page_offset":"0","start_date":"04-01-2020"}\n'
+    response = client.get('/get_job')
+    assert response.status_code == 200
+    assert response.data == b'{"end_date":"04-01-2020","job_id":"2","page_offset":"0","start_date":"04-01-2020"}\n'
 
 
 def test_return_result_success(client):
-    json_data = {'title': u'foo'}
+    json_data = {
+        'client_id': 'client1',
+        'job_id': 'job1',
+        'data': [
+            {
+                'folder_name': 'thisisafoldername',
+                'file_name': 'thisisafilename',
+                'data': {}
+            }
+        ],
+        'jobs': [
+            {
+                'job_type': 'documents',
+                'job_id': 'thisiajobid',
+                'page_offset': 'thisiapageoffset',
+                'start_date': 'thisisastartdate',
+                'end_date': 'thisisanenddate'
+            },
+        ]
+    }
     result = client.post('/return_result', data=json.dumps(json_data),
                          content_type='application/json')
     assert result.status_code == 200
