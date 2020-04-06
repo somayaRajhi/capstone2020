cd518d94a279ec6d89d6d385303570ed65b50b76
Nischal <piyanischal@gmail.com>
2020-04-06 13:38:28 -0400

    Change to return value as a single json

--- tests/c20_client/test_docket_packager.py
+++ tests/c20_client/test_docket_packager.py
@@ -1,11 +1,10 @@
 import pytest
 from c20_client import docket_packager
 
-TEST_JSON = [{
+TEST_JSON = {
     "agencyAcronym": "test",
     "docketId": "123",
-    "data": "STUFF"
-}]
+}
 
 
 @pytest.fixture(name='get_docket')
@@ -15,7 +14,7 @@ def fixture_get_docket():
 
 
 def test_get_docket(get_docket):
-    assert get_docket == [{
+    assert get_docket == {
         'client_id': 1,
         'data': [{
             'folder_name': 'test/123/',
@@ -25,33 +24,30 @@ def test_get_docket(get_docket):
                 'docketId': '123',
                 'file_contents': {
                     "agencyAcronym": "test",
-                    "docketId": "123",
-                    "data": "STUFF"
+                    "docketId": "123"
                 }
             }
         }]
-    }]
+    }
 
 
 def test_client_id(get_docket):
-    assert get_docket[0]['client_id'] == 1
+    assert get_docket['client_id'] == 1
 
 
 def test_folder_name(get_docket):
-    assert get_docket[0]['data'][0]['folder_name'] == 'test/123/'
+    assert get_docket['data'][0]['folder_name'] == 'test/123/'
 
 
 def test_file_name(get_docket):
-    assert get_docket[0]['data'][0]['file_name'] == 'basic_docket.json'
+    assert get_docket['data'][0]['file_name'] == 'basic_docket.json'
 
 
 def test_agency(get_docket):
-    assert get_docket[0]['data'][0]['data']['agency'] == 'test'
+    assert get_docket['data'][0]['data']['agency'] == 'test'
 
 
 def test_docket_id(get_docket):
-    assert get_docket[0]['data'][0]['data']['docketId'] == '123'
+    assert get_docket['data'][0]['data']['docketId'] == '123'
 
 
-def test_file_contents(get_docket):
-    assert get_docket[0]['data'][0]['data']['file_contents']['data'] == 'STUFF'
