a122be86f8d21e6d9ec7417a7a235999ecd5b755
BAnderson6895 <BAnderson6895@gmail.com>
2020-04-06 14:31:35 -0400

    Test handle_jobs

--- tests/test_job_translator.py
+++ tests/test_job_translator.py
@@ -1,14 +1,17 @@
-import pytest
 import json
+import pytest
 from c20_server import job
 from c20_server import job_translator
 from c20_server import job_translator_errors
 
-
-json_result_example = \
+JSON_RESULT_EXAMPLE = \
     {
         "Data": [
-            "..."
+            {
+                "folder_name": "this_is_a_folder_name",
+                "file_name": "this_is_a_file_name",
+                "contents": {}
+            }
         ],
         "jobs": [
             {
@@ -20,7 +23,7 @@ json_result_example = \
             },
         ]
     }
-json_result_example = json.dumps(json_result_example)
+JSON_RESULT_EXAMPLE = json.dumps(JSON_RESULT_EXAMPLE)
 
 
 def test_empty_return():
@@ -44,5 +47,19 @@ def test_single_job():
         "start_date": "this_is_a_start_date",
         "end_date": "this_is_an_end_date"
     }
-    assert job_translator.json_to_job(json_example) == \
-        job.DocumentsJob("this_is_a_job_id", "this_is_a_page_offset", "this_is_a_start_date", "this_is_an_end_date")
+    document_job = job.DocumentsJob("this_is_a_job_id",
+                                    "this_is_a_page_offset",
+                                    "this_is_a_start_date",
+                                    "this_is_an_end_date")
+    assert job_translator.json_to_job(json_example) == document_job
+
+
+def test_handle_single_job():
+    job_list = [
+        job.DocumentsJob(
+            job_id='this_is_a_job_id',
+            page_offset='this_is_a_page_offset',
+            start_date='this_is_a_start_date',
+            end_date='this_is_an_end_date')
+    ]
+    assert job_translator.handle_jobs(JSON_RESULT_EXAMPLE) == job_list
