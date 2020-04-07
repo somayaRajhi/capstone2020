4dc29e4c5f17690657598ff685a7a9fb30e31f45
Tim <fauxt@moravian.edu>
2020-04-07 15:32:39 -0400

    add functionality for the download endpoint

--- tests/c20_client/test_download.py
+++ tests/c20_client/test_download.py
@@ -1,27 +1,10 @@
 import pytest
 import requests_mock
-from c20_client import document_download
+from c20_client import get_download
 from c20_client import reggov_api_doc_error
 
-URL = "https://api.data.gov:443/regulations/v3/document.json?"
-DOC_ID = "EPA-HQ-OAR-2011-0028-0108"
+URL = "https://api.data.gov:443/regulations/v3/download.json?documentId=EPA-HQ-OAR-2011-0028-0108&contentType=pdf"
 API_KEY = "12345"
-
-
-def test_mock_response():
-    with requests_mock.Mocker() as mock:
-        mock.get(URL, json='document received')
-        response = document_download.download_document(API_KEY, DOC_ID)
-        assert response == 'document received'
-
-
-def test_incorrect_id_pattern():
-    with requests_mock.Mocker() as mock:
-        mock.get(URL, json={'a': 'b'},
-                 status_code=400)
-        with pytest.raises(reggov_api_doc_error.IncorrectIDPatternException):
-            bad_pattern = 'b4d' + DOC_ID + 'b4d'
-            document_download.download_document(API_KEY, bad_pattern)
 
 
 def test_incorrect_api_key():
@@ -29,16 +12,7 @@ def test_incorrect_api_key():
         mock.get(URL, json={'a': 'b'},
                  status_code=403)
         with pytest.raises(reggov_api_doc_error.IncorrectApiKeyException):
-            document_download.download_document('INVALID', DOC_ID)
-
-
-def test_bad_document_id():
-    with requests_mock.Mocker() as mock:
-        bad_id = DOC_ID + "-0101"
-        mock.get(URL, json={'a': 'b'},
-                 status_code=404)
-        with pytest.raises(reggov_api_doc_error.BadDocIDException):
-            document_download.download_document(API_KEY, bad_id)
+            get_download.download_file('INVALID', URL)
 
 
 def test_exceed_call_limit():
@@ -46,4 +20,11 @@ def test_exceed_call_limit():
         mock.get(URL, json={'a': 'b'},
                  status_code=429)
         with pytest.raises(reggov_api_doc_error.ExceedCallLimitException):
-            document_download.download_document(API_KEY, DOC_ID)
+            get_download.download_file(API_KEY, URL)
+
+
+def test_file_downloaded():
+    with requests_mock.Mocker() as mock:
+        mock.get(URL, json='file received')
+        response = get_download.download_file(API_KEY, URL)
+        assert response == 'file received'
