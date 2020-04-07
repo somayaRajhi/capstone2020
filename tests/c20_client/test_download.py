3441f6ebc1800f942edc6bd75811077efd5c02d1
Tim <fauxt@moravian.edu>
2020-04-07 15:51:48 -0400

    fix tests

--- tests/c20_client/test_download.py
+++ tests/c20_client/test_download.py
@@ -4,12 +4,13 @@ from c20_client import get_download
 from c20_client import reggov_api_doc_error
 
 URL = "https://api.data.gov:443/regulations/v3/download.json?documentId=EPA-HQ-OAR-2011-0028-0108&contentType=pdf"
+BAD_URL = "https://api.data.gov:443/regulations/v3/download.json?documentId=EPA-HQ-OAR-2011-0000-0108&contentType=pdf"
 API_KEY = "12345"
 
 
 def test_incorrect_api_key():
     with requests_mock.Mocker() as mock:
-        mock.get(URL, json={'a': 'b'},
+        mock.get(URL, text='file received',
                  status_code=403)
         with pytest.raises(reggov_api_doc_error.IncorrectApiKeyException):
             get_download.download_file('INVALID', URL)
@@ -17,14 +18,22 @@ def test_incorrect_api_key():
 
 def test_exceed_call_limit():
     with requests_mock.Mocker() as mock:
-        mock.get(URL, json={'a': 'b'},
+        mock.get(URL, text='file received',
                  status_code=429)
         with pytest.raises(reggov_api_doc_error.ExceedCallLimitException):
             get_download.download_file(API_KEY, URL)
 
 
+def test_bad_url_id():
+    with requests_mock.Mocker() as mock:
+        mock.get(BAD_URL, text='file received',
+                 status_code=404)
+        with pytest.raises(reggov_api_doc_error.BadDocIDException):
+            get_download.download_file(API_KEY, BAD_URL)
+
+
 def test_file_downloaded():
     with requests_mock.Mocker() as mock:
-        mock.get(URL, json='file received')
+        mock.get(URL, text='file received')
         response = get_download.download_file(API_KEY, URL)
-        assert response == 'file received'
+        assert response.content == b'file received'
