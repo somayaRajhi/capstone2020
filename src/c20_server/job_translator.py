987f91f39da7bbeee7d194f73af43bd28110dbdf
BAnderson6895 <BAnderson6895@gmail.com>
2020-04-08 17:23:54 -0400

    Add job_type in job_to_json() function based on type of job object passed to it

--- src/c20_server/job_translator.py
+++ src/c20_server/job_translator.py
@@ -3,16 +3,26 @@ import uuid
 from c20_server import job_translator_errors
 from c20_server.job import DocumentsJob
 
-
-DOCUMENTS = 'documents'
-DOCUMENT = 'document'
-DOCKET = 'docket'
-DOWNLOAD = 'download'
-NONE = 'none'
+DOCUMENTS = "documents"
+DOCUMENT = "document"
+DOCKET = "docket"
+DOWNLOAD = "download"
+NONE = "none"
 
 
 def job_to_json(job_object):
-    return job_object._asdict()
+    job_type = "none"
+    if isinstance(job_object, DocumentsJob):
+        job_type = DOCUMENTS
+    return encode_job(job_type, job_object)
+
+
+def encode_job(job_type, job_object):
+    json_data = job_object._asdict()
+    if job_type == DOCUMENTS:
+        json_data["job_type"] = "documents"
+    json_job = json.dumps(json_data)
+    return json_job
 
 
 def handle_jobs(json_data):
@@ -28,7 +38,7 @@ def handle_jobs(json_data):
     except TypeError:
         return {}
 
-    json_jobs = json_data['jobs']
+    json_jobs = json_data["jobs"]
 
     index = 0
     while index < len(json_jobs):
@@ -40,13 +50,13 @@ def handle_jobs(json_data):
 
 def json_to_job(json_job):
     job_id = str(uuid.uuid4())
-    json_job['job_id'] = job_id
-    job_type = json_job['job_type']
+    json_job["job_id"] = job_id
+    job_type = json_job["job_type"]
 
     if job_type == DOCUMENTS:
-        page_offset = json_job['page_offset']
-        start_date = json_job['start_date']
-        end_date = json_job['end_date']
+        page_offset = json_job["page_offset"]
+        start_date = json_job["start_date"]
+        end_date = json_job["end_date"]
         return DocumentsJob(job_id, page_offset, start_date, end_date)
 
     raise job_translator_errors.UnrecognizedJobTypeException
