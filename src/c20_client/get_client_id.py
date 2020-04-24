087f2b2ca6dc4ca6682e6b12f4993dfc58267263
anthonyestephan <estephana02@moravian.edu>
2020-04-24 13:11:41 -0400

    add function to check if client variable exist in environment

---
+++ src/c20_client/get_client_id.py
@@ -0,0 +1,30 @@
+"""
+File used to check for an environment variable for id and to
+get an id form server and assign the id to the enviornment
+variable if the environment variable does not yet exist
+"""
+from os import getenv
+import requests
+
+
+def request_id():
+    """
+    Ask Moravian server endpoint for id
+    Returns an id
+    """
+    response = requests.get('http://capstone.cs.moravian.edu/get_client_id')
+    return response.json()['client_id']
+
+
+def client_has_id():
+    """
+    Checks for the client id as a .env variable called CLIENT_ID
+    """
+    client_id = getenv("CLIENT_ID")
+
+     # Client does not yet have an id
+    if client_id is None:
+        return False
+
+    # Client has an id assigned
+    return True
