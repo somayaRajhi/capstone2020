864cdf47f367e839ddd22424c4c138698d2015b6
anthonyestephan <estephana02@moravian.edu>
2020-04-24 13:33:56 -0400

    test write to a file

--- tests/c20_client/test_get_id.py
+++ tests/c20_client/test_get_id.py
@@ -1,9 +1,9 @@
 import requests_mock
-from mock import mock_open, patch, MagicMock
+from unittest.mock import mock_open
 import pytest
-from c20_client.get_client_id import request_id, client_has_id
+from c20_client.get_client_id import request_id, save_client_env_variable
 
-CLIENT_ID = 1234
+CLIENT_ID = '1234'
 
 def test_request_id_call():
     with requests_mock.Mocker() as mock:
@@ -16,3 +16,9 @@ def test_request_id_call():
         assert len(history) == 1
         assert 'capstone' in history[0].url
         assert client_id == CLIENT_ID
+
+def test_write_env_variable(mocker):
+    file_mock = mocker.patch('c20_client.get_client_id.open', mock_open())
+    save_client_env_variable(CLIENT_ID)
+    file_mock.assert_called_once_with('.env', 'a+')
+    file_mock().write.assert_called_once_with("CLIENT_ID=" + CLIENT_ID + "\n")
