d2d22b047dc0058e433f17c130b4ca517df85ba9
Ben Coleman <colemanb@moravian.edu>
2020-04-26 23:32:28 -0400

    Ensure folder exists before writing file

--- src/c20_server/data_repository.py
+++ src/c20_server/data_repository.py
@@ -1,10 +1,18 @@
 import os
+from pathlib import Path
 
 
 class DataRepository:
 
-    @staticmethod
-    def save_data(directory_name, filename, contents):
-        path = os.path.join('data', directory_name, filename)
+    def __init__(self, base_path):
+        self.base_path = base_path
+
+    def save_data(self, directory_name, filename, contents):
+        self.ensure_directory_exists(directory_name)
+        path = os.path.join(self.base_path, directory_name, filename)
         with open(path, 'w') as file:
             file.write(contents)
+
+    def ensure_directory_exists(self, directory_name):
+        Path(os.path.join(self.base_path, directory_name))\
+            .mkdir(parents=True, exist_ok=True)
--- tests/test_data_repository.py
+++ tests/test_data_repository.py
@@ -3,9 +3,18 @@ from unittest.mock import mock_open
 from c20_server.data_repository import DataRepository
 
 
+class DummyMakeDirDataRepository(DataRepository):
+    def __init__(self, base_path):
+        super().__init__(base_path)
+        self.ensure_directory_exists_called = False
+
+    def ensure_directory_exists(self, directory_name):
+        self.ensure_directory_exists_called = True
+
+
 def test_class_saves_data(mocker):
     file_mock = mocker.patch('c20_server.data_repository.open', mock_open())
-    data_repo = DataRepository()
+    data_repo = DummyMakeDirDataRepository('data')
     directory = 'FAA/DOC-ID/'
     filename = 'document.json'
     contents = 'foo\nbar\n'
@@ -13,3 +22,4 @@ def test_class_saves_data(mocker):
 
     file_mock.assert_called_once_with('data/FAA/DOC-ID/document.json', 'w')
     file_mock().write.assert_called_once_with(contents)
+    assert data_repo.ensure_directory_exists_called
