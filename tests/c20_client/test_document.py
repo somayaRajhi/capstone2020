139190b28b254d8e833b2f6aa9e8b2d16b55323b
Sara-dahdoh <60446896+Sara-dahdoh@users.noreply.github.com>
2020-04-28 22:15:50 -0400

    typo fix

--- tests/c20_client/test_document.py
+++ tests/c20_client/test_document.py
@@ -25,7 +25,7 @@ def test_incorrect_id_pattern():
         with pytest.raises(reggov_api_doc_error.IncorrectIDPatternException):
             bad_pattern = 'b4d' + DOC_ID + 'b4d'
             get_document.download_document(API_KEY, bad_pattern)
-            result = handling_erorr(URL+API_KEY, bad_pattern, massage_report=":received 400:Bad request")
+            result = handling_erorr(URL+API_KEY, bad_pattern, message_report=":received 400:Bad request")
             mock.post('http://capstone.cs.moravian.edu/report_failure',
                       json={'client_id': CLIENT_ID,
                             'job_id': JOB_ID,
@@ -39,7 +39,7 @@ def test_incorrect_api_key():
         with pytest.raises(reggov_api_doc_error.IncorrectApiKeyException):
             get_document.download_document('INVALID', DOC_ID)
             result = handling_erorr(URL +'INVALID', DOC_ID
-                                    , massage_report=":received 403:Forbidden")
+                                    , message_report=":received 403:Forbidden")
             mock.post('http://capstone.cs.moravian.edu/report_failure',
                       json={'client_id': CLIENT_ID,
                             'job_id': JOB_ID,
@@ -54,7 +54,7 @@ def test_bad_document_id():
         with pytest.raises(reggov_api_doc_error.BadDocIDException):
             get_document.download_document(API_KEY, bad_id)
             result = handling_erorr(URL + API_KEY, bad_id
-                                    , massage_report=":received 404:Not Found")
+                                    , message_report=":received 404:Not Found")
             mock.post('http://capstone.cs.moravian.edu/report_failure',
                       json={'client_id': CLIENT_ID,
                             'job_id': JOB_ID,
@@ -68,7 +68,7 @@ def test_exceed_call_limit():
         with pytest.raises(reggov_api_doc_error.ExceedCallLimitException):
             get_document.download_document(API_KEY, DOC_ID)
             result = handling_erorr(URL + API_KEY, DOC_ID
-                                    , massage_report=":received 429:"
+                                    , message_report=":received 429:"
                                                      "Too Many Requests")
             mock.post('http://capstone.cs.moravian.edu/report_failure',
                       json={'client_id': CLIENT_ID,
