ffe3a33b2d793425faad330acaca8907a1684bfe
riyadgh <riyad.progs@gmail.com>
2020-04-29 08:41:43 -0400

    Move the user_ids methods to be in its own class

---
+++ src/c20_server/user_manager.py
@@ -0,0 +1,21 @@
+
+class UserManager:
+
+    def __init__(self, database):
+        self.r_database = database
+
+    def initialize_user_ids(self):
+        if self.r_database.get('user_id') is None:
+            self.r_database.set('user_id', 0)
+
+    # def set_new_user_id(self, id_number):
+    #     self.r_database.set('user_id', id_number)
+
+    def get_new_user_id(self):
+        """
+        Calling this method will first check if the 'user_id' has been set
+         or not, then it will increment the id # by redis feature using .incr()
+        """
+        self.initialize_user_ids()
+        new_user_id = self.r_database.incr('user_id')
+        return new_user_id
